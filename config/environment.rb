fde7d7a7680d99fdd225d587f073ccaed3549e31
Raymond Belonia <raymondbelonia@gmail.com>
2014-04-22 06:06:44 +0000

    Added Basic Mailer for received and shipped orders

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+#Configure mailer
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings ={
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "sampleapplicationzero@gmail.com",
+    password: "zerospades25",
+    enable_starttls_auto: true
+    }
+end
+
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -10,14 +10,12 @@ class OrdersControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:orders)
   end
-  
-  test "requires items in cart" do
+
+  test "requires item in cart" do
     get :new
     assert_redirected_to store_path
-    assert_equal_flash[:notice], 'Your cart is empty'
+    assert_equal flash[:notice], 'Your cart is empty'
   end
-  
-  
 
   test "should get new" do
     item = LineItem.new
@@ -25,13 +23,15 @@ class OrdersControllerTest < ActionController::TestCase
     item.product = products(:ruby)
     item.save!
     session[:cart_id] = item.cart.id
+
     get :new
     assert_response :success
   end
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
+      post :create, order: { address: @order.address, email: @order.email,
+        name: @order.name, pay_type: @order.pay_type }
     end
 
     assert_redirected_to store_path
